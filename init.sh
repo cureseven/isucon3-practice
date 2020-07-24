fd573db5b1577c99752d459cc2b9e6deddb3d6ea
cureseven <ogasawara.nana@willgate.co.jp>
2020-07-24 15:34:12 +0900

    findex貼った

--- init.sh
+++ init.sh
@@ -1,3 +1,7 @@
 #!/bin/bash -eu
 
 echo 'init process'
+
+mysql --user=isucon --password=isucon isucon -e "CREATE INDEX `memos_idx_is_private_created_at` ON memos (`is_private`,`created_at`);"
+mysql --user=isucon --password=isucon isucon -e "CREATE INDEX `memos_idx_user_created_at` ON memos (`user`,`created_at`);"
+mysql --user=isucon --password=isucon isucon -e "CREATE INDEX `memos_idx_user_is_private_created_at` ON memos (`user`,`is_private`,`created_at`);"
