596b88346edfe36d7dbd557820f2edcf6027f650
cureseven <ogasawara.nana@willgate.co.jp>
2020-07-24 16:37:18 +0900

    indexはるクエリ修正

--- init.sh
+++ init.sh
@@ -2,6 +2,6 @@
 
 echo 'init process'
 
-mysql --user=isucon --password=isucon isucon -e "CREATE INDEX `memos_idx_is_private_created_at` ON memos (`is_private`,`created_at`);"
-mysql --user=isucon --password=isucon isucon -e "CREATE INDEX `memos_idx_user_created_at` ON memos (`user`,`created_at`);"
-mysql --user=isucon --password=isucon isucon -e "CREATE INDEX `memos_idx_user_is_private_created_at` ON memos (`user`,`is_private`,`created_at`);"
+mysql --user=isucon --password=isucon isucon -e "CREATE INDEX memos_idx_is_private_created_at ON memos (is_private, created_at);"
+mysql --user=isucon --password=isucon isucon -e "CREATE INDEX memos_idx_user_created_at ON memos (user,created_at);"
+mysql --user=isucon --password=isucon isucon -e "CREATE INDEX memos_idx_user_is_private_created_at ON memos (user,is_private,created_at);"
