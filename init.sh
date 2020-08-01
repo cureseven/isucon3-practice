10f99dea5f872eea9d2b3fbc751de40febe91ec5
cureseven <shiningcureseven@gmail.com>
2020-08-01 17:44:20 +0900

    クエリ修正

--- init.sh
+++ init.sh
@@ -6,4 +6,4 @@ mysql --user=isucon --password=isucon isucon -e "CREATE INDEX memos_idx_is_priva
 mysql --user=isucon --password=isucon isucon -e "CREATE INDEX memos_idx_user_created_at ON memos (user,created_at);"
 mysql --user=isucon --password=isucon isucon -e "CREATE INDEX memos_idx_user_is_private_created_at ON memos (user,is_private,created_at);"
 mysql --user=isucon --password=isucon isucon -e "CREATE TABLE isucon.public_memos (id int(11) auto_increment,memo_id int(11),PRIMARY KEY (id));"
-mysql --user=isucon --password=isucon isucon -e "INSERT INTO public_memos (memo_id) SELECT id FROM memos WHERE is_private=0 ORDER BY created_at DESC"
+mysql --user=isucon --password=isucon isucon -e "INSERT INTO public_memos (memo_id) SELECT id FROM memos WHERE is_private=0 ORDER BY created_at DESC;"
